2011ad050cb36027179a8c8f32a49fb4344d418c
RashmiChandran <rashmichandran3@gmail.com>
2021-06-09 13:00:01 +0530

    Added disabled style to decrement button

--- src/Components/Products/Product.js
+++ src/Components/Products/Product.js
@@ -15,10 +15,12 @@ const Product = ({ product, addToCart, removeFromCart, incrementItem, decrementI
       <p>{productItem.productName}</p>
       <p>{productItem.productPrice}</p>
       {productItem.isAddedtoCart ? (
-        <div className="flex">
-         {productItem.count > 1 ? <FaMinusSquare onClick={()=> decrementItem(productItem)} /> : null}
-         {productItem.count}
-          {productItem.count >= 1 ?<FaPlusSquare onClick={()=>incrementItem(productItem)}/> : null}
+        <div className="flex grid-cols-2 gap-2 grid w-4/5">
+          <div className="flex items-center">
+           <button  onClick={()=> decrementItem(productItem)} disabled={productItem.count <= 1 } className="disabled:opacity-50 disabled:cursor-text"><FaMinusSquare className="w-5 h-5" /> </button>
+            <span className="mx-2">{productItem.count}</span>
+            {productItem.count >= 1 ? <button onClick={()=>incrementItem(productItem)}><FaPlusSquare  className="w-5 h-5"/></button> : null}
+          </div>
           <button onClick={() => removeFromCart(productItem)} className="btn btn-red">
             <FaTrashAlt  />
           </button>
